6c3a7e68265034d9ca8db5324957fad80314712a
Kannnan04 <kannansiva0910@gmail.com>
2024-01-06 22:42:43 +0530

    for-of updated

---
+++ for-of.js
@@ -0,0 +1,20 @@
+let myData =[ {
+    firstName : "Kannan",
+    lastName : "S",
+    eMail : "kannansiva0910@gmail.com",
+    location : "neyveli",
+    qualification : "B.E",
+    passedOut : "2023",
+    frontEnd: "HTML, CSS, BS, JS"
+}]
+
+// let myArray = [];
+
+for (val of myData){
+// let op = []
+// op.push(key);
+// op.push(myData(key))
+// myArray.push(op)
+
+}
+console.log(val)
