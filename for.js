45f40804cc6531902de1548cf7fcfd60299dc822
Kannnan04 <kannansiva0910@gmail.com>
2024-01-06 23:00:27 +0530

    for updated

---
+++ for.js
@@ -0,0 +1,13 @@
+let myData =[ {
+    firstName : "Kannan",
+    lastName : "S",
+    eMail : "kannansiva0910@gmail.com",
+    location : "neyveli",
+    qualification : "B.E",
+    passedOut : "2023",
+    frontEnd: "HTML, CSS, BS, JS"
+}]
+
+for(let i=0; i <= myData.length; i++){
+    console.log(i)
+}
